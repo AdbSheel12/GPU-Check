fd7636f0204b80cfeb769ab2a7404c43b47df888
AdbShell12 <AdbShell12@example.com>
2024-04-09 12:23:35 +0700

    GPU.py

---
+++ GPU.py
@@ -0,0 +1,58 @@
+clear
+blue='\033[34:1m'
+red='\033[31;1m'
+cyan='\033[36;1m'
+green='\033[32;1m'
+sleep 2
+echo $cyan"Jagan Lupa Subrek Akun YT"$green"Satria"
+sleep 3
+echo
+echo $cyan
+echo figlet GPU
+sleep 1
+echo $blue"Shilakan Pilih Jenis GPU"
+sleep 1
+echo $green"1.)OpenGels2.0"
+sleep 1
+echo $green"2.)OpenGels3.2"
+sleep 1
+echo $green"3.)Vulkan3.0"
+echo $cyan
+read -p "Pilih Mana : " Bang
+if [ $Bang = 1 ] || [ $Bang = 1 ]
+then
+sleep 1
+toilet -f big -F GPU Chacking...
+print $green"GPU Opengels2.0 Check...."
+sleep 2
+echo $blue"Check done [✓]
+sleep 2
+exit
+fi
+
+if [ $Bang = 2 ] || [ $Bang = 2]
+then
+sleep 1
+toilet -f big -F GPU Chacking...
+echo $green"GPU OpenGels3.2 Check.."
+sleep 3
+print $cyan"Check Done [✓]"
+sleep 1
+exit
+fi
+
+if [ $Bang = 3 ] || [ $Bang = 3 ]
+then
+sleep 1
+toilet -f big -F GPU Chacking...
+sleep 2
+echo $blue"GPU Vulkan3.0 Check"
+sleep 2
+print $green"Check Done [✓]"
+sleep 2
+clear
+sleep 1
+echo $blue"Terimakasih telah memakai GPU Checker Milik kami Bayyyy"
+sleep 1
+exit
+fi
